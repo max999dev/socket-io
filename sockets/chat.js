ef21d73c269a2d2a393089dc8d75ce8679035df4
Max <sveramaxim@gmail.com>
2021-06-16 21:06:26 -0500

    Let's send some messages

--- sockets/chat.js
+++ sockets/chat.js
@@ -5,4 +5,10 @@ module.exports = (io, socket) => {
     console.log(`${username} has joined the chat`);
     io.emit('new user', username);
   });
+  //Listen for new messages
+  socket.on('new message', data => {
+    // Send that data back to ALL clients
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`);
+    io.emit('new message', data);
+  });
 };
